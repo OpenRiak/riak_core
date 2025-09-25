7cafde915296ce4e97088a2e23b417da1c37d426
Martin Sumner <martin.sumner@adaptip.co.uk>
2025-09-25 16:51:08 +0100

    Make non-legacy routing permanent

--- src/riak_core_vnode_master.erl
+++ src/riak_core_vnode_master.erl
@@ -133,13 +133,8 @@ coverage(Msg, {Index, Node}, Keyspaces, Sender, VMaster) ->
 %% VnodePid}'.
 command_return_vnode({Index,Node}, Msg, Sender, VMaster) ->
     Req = make_request(Msg, Sender, Index),
-    case riak_core_capability:get({riak_core, vnode_routing}, legacy) of
-        legacy ->
-            gen_server:call({VMaster, Node}, {return_vnode, Req}, ?LONG_TIMEOUT);
-        proxy ->
-            Mod = vmaster_to_vmod(VMaster),
-            riak_core_vnode_proxy:command_return_vnode({Mod,Index,Node}, Req)
-    end.
+    Mod = vmaster_to_vmod(VMaster),
+    riak_core_vnode_proxy:command_return_vnode({Mod,Index,Node}, Req).
 
 %% Send a synchronous command to an individual Index/Node combination.
 %% Will not return until the vnode has returned
